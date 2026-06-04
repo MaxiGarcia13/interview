d767e45c60ca0795c08378a2d23471a9c72bb162
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-06-04 11:49:08 +0200

    feat: implement input component with suggestions and debounce functionality

---
+++ src/utils/debounce.ts
@@ -0,0 +1,12 @@
+export function debounce<T extends (...args: Parameters<T>) => void>(fn: T, delay: number) {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return function (...args: Parameters<T>) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn(...args);
+      console.log('fn was called');
+    }, delay);
+  };
+}
